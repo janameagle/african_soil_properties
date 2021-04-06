488efbe85959872c9d785f8c388a603fcefe9faa
janameagle <jana.maier@mail.de>
2021-04-06 14:06:37 +0200

    bulk density for some countries

--- data_preparation.R
+++ data_preparation.R
@@ -11,12 +11,6 @@ library(raster)
 # get vector files of the WASCAL countries
 countries <- readOGR("C:/Users/jmaie/Documents/WASCAL-DE_Coop/african_soil_properties/data/naturalearthdata", "ne_10m_admin_0_countries")
 
-head(countries)
-unique(countries$SOVEREIGNT)
-unique(countries$ADMIN)
-names(countries)
-countries2 <- countries[, c("scalerank", "SOVEREIGNT")]
-
 coi <- c("Benin", "Burkina Faso", "Cabo Verde", "Ivory Coast", "Gambia", 
          "Ghana", "Mali", "Niger", "Nigeria", "Senegal", "Togo")
 
@@ -26,42 +20,72 @@ coi <- c("Benin", "Burkina Faso", "Cabo Verde", "Ivory Coast", "Gambia",
 # unique(test$SOVEREIGNT)
 
 aoi <- countries[countries$ADMIN %in% coi, ]
+benin <- countries[countries$ADMIN == "Benin", ]
+burkinafaso <- countries[countries$ADMIN == "Burkina Faso", ]
+caboverde <- countries[countries$ADMIN == "Cabo Verde", ]
+ivorycoast <- countries[countries$ADMIN == "Ivory Coast", ]
+gambia <- countries[countries$ADMIN == "Gambia", ]
 ghana <- countries[countries$ADMIN == "Ghana", ]
-nrow(ghana)
-nrow(aoi)
-unique(aoi$ADMIN)
+mali <- countries[countries$ADMIN == "Mali", ]
+niger <- countries[countries$ADMIN == "Niger", ]
+nigeria <- countries[countries$ADMIN == "Nigeria", ]
+senegal <- countries[countries$ADMIN == "Senegal", ]
+togo <- countries[countries$ADMIN == "Togo", ]
 
-# using the terra package to create a new SpatVector
-aoi.v <- vect(aoi)
-class(aoi.v)     
 
-ghana.v <- vect(ghana)
 
 ############################
 # get the soil bulk density
 # access and crop 
-# tif.bd <- paste0("/vsicurl/https://s3.eu-central-1.wasabisys.com/africa-soil/layers30m/",
-#                  c("sol_db_od_m_30m_0..20cm_2001..2017_v0.13_wgs84.tif",
-#                    "sol_db_od_m_30m_20..50cm_2001..2017_v0.13_wgs84.tif",
-#                    "sol_db_od_md_30m_0..20cm_2001..2017_v0.13_wgs84.tif"))
-# tif.bd
+tif.bd <- paste0("/vsicurl/https://s3.eu-central-1.wasabisys.com/africa-soil/layers30m/", 
+                 c("sol_db_od_m_30m_0..20cm_2001..2017_africa_epsg4326_v0.1.tif",
+                   "sol_db_od_m_30m_20..50cm_2001..2017_africa_epsg4326_v0.1.tif"))
+
+tif.bd
 # md data is the associated prediction error
 
 # load the values
-# bd30m <- lapply(tif.bd, function(i){crop(rast(i), aoi.v)})
-# bd30m.sp = do.call(cbin, lapply(bd30m, function(i){as.data.frame(i)}))
-# str(sol30m.sp)
-# 
-# rast("/vsicurl/https://s3.eu-central-1.wasabisys.com/africa-soil/layers30m/sol_db_od_m_30m_0..20cm_2001..2017_v0.13_wgs84.tif")
+#bd30m_all <- rast(tif.bd) # get the different rasters from online
+bd30m_all <- lapply(tif.bd, function(i){rast(i)})
 
-bld = "/vsicurl/https://s3.eu-central-1.wasabisys.com/africa-soil/layers30m/sol_db_od_m_30m_0..20cm_2001..2017_africa_epsg4326_v0.1.tif"
+# using the terra package to create a new SpatVector
+aoi <- caboverde # could be replaced by for loop
+coi <- c("Niger", "Nigeria", "Senegal", "Togo")
+aoi.v <- vect(aoi) # create SpatVector
 
-bld30m = crop(rast(bld), ghana.v)
-bld30m.sp = as.data.frame(bld30m)
+# crop the data
+#bd30m_ctr <- crop(bd30m_all, aoi.v)
+
+for (k in coi) {
+  print(k)
+  aoi <- countries[countries$ADMIN == k, ]
+  aoi.v <- vect(aoi) # create SpatVector
+  print("vector created")
+  bd30m_ctr <- do.call(cbind, lapply(bd30m_all, function(i){crop(i, aoi.v)}))
+  print("cropped")
+  mask_ctr <- rasterize(aoi.v, bd30m_ctr[[1]])
+  bd30m_ctr_msk <- lapply(bd30m_ctr, function(i){mask(i, mask_ctr)})
+  print("masked")
+
+  plot(bd30m_ctr[[1]])
+  plot(bd30m_ctr_msk[[1]])
+
+# save the new cropped raster
+  print("writing files")
+  writeRaster(bd30m_ctr[[1]], paste0("output/", k, "/bulk_density_0m_20m_", k, ".tif"))
+  writeRaster(bd30m_ctr[[2]], paste0("output/", k, "/bulk_density_20m_50m_", k, ".tif"))
+  writeRaster(bd30m_ctr_msk[[1]], paste0("output/", k, "/bulk_density_0m_20m_", k, "_masked.tif"))
+  writeRaster(bd30m_ctr_msk[[2]], paste0("output/", k, "/bulk_density_20m_50m_", k, "_masked.tif"))
+  print("country done")
+}
+
+
+# bld30m.sp = as.data.frame(bld30m)
 
 # values are in 10*kg/m3
 # bld30m.sp$bld_l1 = bld30m.sp$sol_db_od_m_30m_0..20cm_2001..2017_africa_epsg4326_v0.1 * 10
 
+class(bld30m)
 plot(bld30m)
 
 
