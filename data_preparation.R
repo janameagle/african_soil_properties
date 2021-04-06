23210cb62f93521612512401feb0d6f60136977f
janameagle <jana.maier@mail.de>
2021-04-06 08:45:41 +0200

    initial load

---
+++ data_preparation.R
@@ -0,0 +1,10 @@
+################################################################################
+#### Soil property data will be downloaded for all WASCAL countries ############
+#### data details: https://zenodo.org/search?page=1&size=20&q=iSDAsoil #########
+#### workflow: https://gitlab.com/openlandmap/africa-soil-and-agronomy-data-cube
+################################################################################
+
+library(rgdal)
+library(terra)
+library(raster)
+
