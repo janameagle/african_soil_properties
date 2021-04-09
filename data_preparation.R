c079993138014acb9a43b632e8629cd49110df7c
janameagle <jana.maier@mail.de>
2021-04-09 09:44:37 +0200

    new loop

--- data_preparation.R
+++ data_preparation.R
@@ -19,63 +19,116 @@ coi <- c("Benin", "Burkina Faso", "Cabo Verde", "Ivory Coast", "Gambia",
 # unique(test$ADMIN)
 # unique(test$SOVEREIGNT)
 
-aoi <- countries[countries$ADMIN %in% coi, ]
-benin <- countries[countries$ADMIN == "Benin", ]
-burkinafaso <- countries[countries$ADMIN == "Burkina Faso", ]
-caboverde <- countries[countries$ADMIN == "Cabo Verde", ]
-ivorycoast <- countries[countries$ADMIN == "Ivory Coast", ]
-gambia <- countries[countries$ADMIN == "Gambia", ]
-ghana <- countries[countries$ADMIN == "Ghana", ]
-mali <- countries[countries$ADMIN == "Mali", ]
-niger <- countries[countries$ADMIN == "Niger", ]
-nigeria <- countries[countries$ADMIN == "Nigeria", ]
-senegal <- countries[countries$ADMIN == "Senegal", ]
-togo <- countries[countries$ADMIN == "Togo", ]
+# aoi <- countries[countries$ADMIN %in% coi, ]
+# benin <- countries[countries$ADMIN == "Benin", ]
+# burkinafaso <- countries[countries$ADMIN == "Burkina Faso", ]
+# caboverde <- countries[countries$ADMIN == "Cabo Verde", ]
+# ivorycoast <- countries[countries$ADMIN == "Ivory Coast", ]
+# gambia <- countries[countries$ADMIN == "Gambia", ]
+# ghana <- countries[countries$ADMIN == "Ghana", ]
+# mali <- countries[countries$ADMIN == "Mali", ]
+# niger <- countries[countries$ADMIN == "Niger", ]
+# nigeria <- countries[countries$ADMIN == "Nigeria", ]
+# senegal <- countries[countries$ADMIN == "Senegal", ]
+# togo <- countries[countries$ADMIN == "Togo", ]
 
 
 
 ############################
-# get the soil bulk density
+ # get the soil bulk density
 # access and crop 
-tif.bd <- paste0("/vsicurl/https://s3.eu-central-1.wasabisys.com/africa-soil/layers30m/", 
-                 c("sol_db_od_m_30m_0..20cm_2001..2017_africa_epsg4326_v0.1.tif",
-                   "sol_db_od_m_30m_20..50cm_2001..2017_africa_epsg4326_v0.1.tif"))
+tif20.bd <- paste0("/vsicurl/https://s3.eu-central-1.wasabisys.com/africa-soil/layers30m/", 
+                 c("sol_db_od_m_30m_0..20cm_2001..2017_africa_epsg4326_v0.1.tif"))
 
-tif.bd
+tif50.bd <- paste0("/vsicurl/https://s3.eu-central-1.wasabisys.com/africa-soil/layers30m/", 
+                    c("sol_db_od_m_30m_20..50cm_2001..2017_africa_epsg4326_v0.1.tif"))
+ 
+
 # md data is the associated prediction error
 
+#### manual ##############
+
+
+coi <- "Nigeria"
+aoi.v <- vect(nigeria) # create SpatVector
+
 # load the values
-#bd30m_all <- rast(tif.bd) # get the different rasters from online
-bd30m_all <- lapply(tif.bd, function(i){rast(i)})
+bd30m_all <- rast(tif20.bd) # get the different rasters from online
+#bd30m_all_orig <- lapply(tif.bd, function(i){rast(i)})
 
 # using the terra package to create a new SpatVector
-aoi <- caboverde # could be replaced by for loop
-coi <- c("Niger", "Nigeria", "Senegal", "Togo")
-aoi.v <- vect(aoi) # create SpatVector
+#aoi <- caboverde # could be replaced by for loop
+
 
 # crop the data
-#bd30m_ctr <- crop(bd30m_all, aoi.v)
+ bd30m_ctr <- crop(bd30m_all, aoi.v)
+ ext(bd30m_ctr)
+ mask_ctr <- rasterize(aoi.v, bd30m_ctr)
+ bd30m_ctr_msk <- mask(bd30m_ctr, mask_ctr)
+
+ writeRaster(bd30m_ctr_msk, paste0("output/", coi, "/bulk_density_0m_20m_", coi, "_masked.tif"))
+
+ 
+#### 20 - 50m
+ 
+ bd30m_all <- rast(tif50.bd) # get the different rasters from online 
+ # crop the data
+ bd30m_ctr <- crop(bd30m_all, aoi.v)
+ ext(bd30m_ctr)
+ mask_ctr <- rasterize(aoi.v, bd30m_ctr)
+ bd30m_ctr_msk <- mask(bd30m_ctr, mask_ctr) 
+ 
+ writeRaster(bd30m_ctr_msk, paste0("output/", coi, "/bulk_density_20m_50m_", coi, "_masked.tif"))
+
+ 
+ 
+
+ #### automatic loop ###################
+bd30m_all_20 <- rast(tif20.bd) # get the different rasters from online
+bd30m_all_50 <- rast(tif50.bd) # get the different rasters from online
+ 
+coi <- c("Benin", "Burkina Faso", "Cabo Verde", "Ivory Coast", "Gambia", 
+         "Ghana", "Mali", "Niger", "Nigeria", "Senegal", "Togo")
 
 for (k in coi) {
   print(k)
   aoi <- countries[countries$ADMIN == k, ]
   aoi.v <- vect(aoi) # create SpatVector
+  
+  bd30m_20 <- bd30m_all_20
+  bd30m_50 <- bd30m_all_50
+  
   print("vector created")
-  bd30m_ctr <- do.call(cbind, lapply(bd30m_all, function(i){crop(i, aoi.v)}))
+  print("20m")
+  bd30m_ctr <- crop(bd30m_20, aoi.v)
   print("cropped")
-  mask_ctr <- rasterize(aoi.v, bd30m_ctr[[1]])
-  bd30m_ctr_msk <- lapply(bd30m_ctr, function(i){mask(i, mask_ctr)})
+  print(paste0("Extent:", ext(bd30m_ctr)))
+  mask_ctr <- rasterize(aoi.v, bd30m_ctr)
+  bd30m_ctr_msk <- mask(bd30m_ctr, mask_ctr) 
   print("masked")
 
-  plot(bd30m_ctr[[1]])
-  plot(bd30m_ctr_msk[[1]])
+ # plot(bd30m_ctr[[1]])
+  plot(bd30m_ctr_msk)
 
 # save the new cropped raster
-  print("writing files")
-  writeRaster(bd30m_ctr[[1]], paste0("output/", k, "/bulk_density_0m_20m_", k, ".tif"))
-  writeRaster(bd30m_ctr[[2]], paste0("output/", k, "/bulk_density_20m_50m_", k, ".tif"))
-  writeRaster(bd30m_ctr_msk[[1]], paste0("output/", k, "/bulk_density_0m_20m_", k, "_masked.tif"))
-  writeRaster(bd30m_ctr_msk[[2]], paste0("output/", k, "/bulk_density_20m_50m_", k, "_masked.tif"))
+  print("writing file")
+  writeRaster(bd30m_ctr_msk, paste0("output/", k, "/bulk_density_0m_20m_", k, "_masked.tif"))
+ 
+  print("50m")
+  bd30m_ctr <- crop(bd30m_50, aoi.v)
+  print("cropped")
+  print(paste0("Extent:", ext(bd30m_ctr)))
+  mask_ctr <- rasterize(aoi.v, bd30m_ctr)
+  bd30m_ctr_msk <- mask(bd30m_ctr, mask_ctr) 
+  print("masked")
+  
+  # plot(bd30m_ctr[[1]])
+  plot(bd30m_ctr_msk)
+  
+  # save the new cropped raster
+  print("writing file")  
+  
+  writeRaster(bd30m_ctr_msk, paste0("output/", k, "/bulk_density_20m_50m_", k, "_masked.tif"))
   print("country done")
 }
 
@@ -116,3 +169,16 @@ bd20.m$l1 = bd20.df$l1[bd20.m@grid.index]
 # plot the extractabel bulk density for the aoi
 plot(bd20.ghana)
 spplot(bd20.m$l1)
+
+
+
+
+################################################################################
+#### minimize current files
+getwd()
+myfile <- raster("C:/Users/jmaie/Documents/WASCAL-DE_Coop/african_soil_properties/output/Benin/bulk_density_0m_20m_Benin_masked.tif")
+extent(myfile)
+extent(benin)
+plot(myfile)
+plot(benin)
+class(myfile)
